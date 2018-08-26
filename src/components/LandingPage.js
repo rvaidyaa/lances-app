3cb81683d2751d624fa19fdb7eddadcb2bd27939
Kevin Murphy <midimurphdesigns@gmail.com>
2018-08-26 16:05:00 -0700

    wireframed homepage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
+import "./LandingPage.css";
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div>
+      <div className="landingpage-wrapper">
         <section className="about-section">
           <h2>About</h2>
         </section>
         <section className="how-to-section">
-          <h2>Need Help Now</h2>
+          <h2>How To</h2>
         </section>
         <section className="click-here-section">
-          <div className="">
-            
+          <div className="click-here-resources">
+            <h3>Resources</h3>
           </div>
-          <div className="">
-
+          <div className="click-here-need-help">
+            <h3>Need Help Now</h3>
           </div>
         </section>
       </div>
