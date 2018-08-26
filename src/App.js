7ec6baaf02ac3e328db741b5ae4954ec583122da
Kevin Murphy <midimurphdesigns@gmail.com>
2018-08-26 15:48:29 -0700

    imported landing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <LandingPage />
         <Footer />
       </div>
     );
