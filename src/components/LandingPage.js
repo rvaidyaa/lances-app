87b59f8abb005e73beb3114790684f1ee36b90ff
Kevin Murphy <midimurphdesigns@gmail.com>
2018-08-26 15:48:05 -0700

    made landing page and removed onboarding

---
+++ src/components/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <section className="about-section">
+          <h2>About</h2>
+        </section>
+        <section className="how-to-section">
+          <h2>Need Help Now</h2>
+        </section>
+        <section className="click-here-section">
+          <div className="">
+            
+          </div>
+          <div className="">
+
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
