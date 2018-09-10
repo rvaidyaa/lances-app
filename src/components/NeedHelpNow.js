cdd7d03ee12903577108c6d8cf5d35f1333be9e1
gregsprog <gregsprog@gmail.com>
2018-09-10 10:35:08 -0700

    minor style changes

--- src/components/NeedHelpNow.js
+++ src/components/NeedHelpNow.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import "./NeedHelpNow.css";
 
 export default class NeedHelpNow extends Component {
   render() {
     return (
       <div>
-        <h1>Need Help Now!</h1>
+        <h1>What to do in a mental health crisis</h1>
+        <h2> Call 911 if in a life threatening emergency</h2>
+
       </div>
     );
   }
